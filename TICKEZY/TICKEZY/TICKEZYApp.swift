020ceb6a5ad75995799a3baceff272855e7ab075
Mugabo Andre <andremugabo@yahoo.fr>
2025-10-14 00:34:07 +0200

    initilization of the swift app

---
+++ TICKEZY/TICKEZY/TICKEZYApp.swift
@@ -0,0 +1,17 @@
+//
+//  TICKEZYApp.swift
+//  TICKEZY
+//
+//  Created by M.A on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TICKEZYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TICKEZY/TICKEZYTests/TICKEZYTests.swift
@@ -0,0 +1,17 @@
+//
+//  TICKEZYTests.swift
+//  TICKEZYTests
+//
+//  Created by M.A on 10/14/25.
+//
+
+import Testing
+@testable import TICKEZY
+
+struct TICKEZYTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
